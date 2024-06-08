74a0a23f8795c60a103595184a4ca4af9e327675
KavinduSooriyaarachchi <kavindusooriyaarachchi4@gmail.com>
2024-06-08 13:58:27 +0530

    detail

--- react-firebase-chat-starter/src/components/detail/Detail.jsx
+++ react-firebase-chat-starter/src/components/detail/Detail.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../detail/detail.css";
 
 const Detail = () => {
-  return <div className="detail">Detail</div>;
+  return <div className="detail">Detail b</div>;
 };
 
 export default Detail;
