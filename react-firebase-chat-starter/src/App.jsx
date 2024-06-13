185f13cae069bd2fc1ae881f7c142cb812d87aed
KavinduSooriyaarachchi <kavindusooriyaarachchi4@gmail.com>
2024-06-14 02:39:22 +0530

    React Firebase Auth with Zustand State Management Tool

--- react-firebase-chat-starter/src/App.jsx
+++ react-firebase-chat-starter/src/App.jsx
@@ -8,11 +8,9 @@ import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./lib/firebase";
 
 const App = () => {
-  const user = false;
-
   useEffect(() => {
     const unSub = onAuthStateChanged(auth, (user) => {
-      console.log(user);
+      console.log(user.uid);
     });
 
     return () => {
---
+++ react-firebase-chat-starter/src/lib/userStore.js
@@ -0,0 +1,15 @@
+import { create } from "zustand";
+
+const useUserStore = create((set) => ({
+  currentUser: null,
+  isLoading: true,
+  fetchUserInfo: async (uid) => {
+    if (!uid) return set({ currentUser: null, isLoading: false });
+
+    try {
+    } catch (err) {
+      console.log(err);
+      return set({ currentUser: null, isLoading: false });
+    }
+  },
+}));
