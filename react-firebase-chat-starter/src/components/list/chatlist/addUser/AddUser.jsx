e6cdeb31e5974c1a4186785c30d11b07fad996b6
KavinduSooriyaarachchi <kavindusooriyaarachchi4@gmail.com>
2024-06-14 19:41:22 +0530

    Search a User from Firebase Firestore

--- react-firebase-chat-starter/src/components/list/chatlist/addUser/AddUser.jsx
+++ react-firebase-chat-starter/src/components/list/chatlist/addUser/AddUser.jsx
@@ -1,20 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./addUser.css";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "../../../../lib/firebase";
 
 const AddUser = () => {
+  const [user, setUser] = useState(null);
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const username = formData.get("username");
+
+    try {
+      const userRef = collection(db, "users");
+
+      const q = query(userRef, where("username", "==", username));
+
+      const querySnapShot = await getDocs(q);
+
+      if (!querySnapShot.empty) {
+        setUser(querySnapShot.docs[0].data());
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="addUser">
-      <form action="">
+      <form onSubmit={handleSearch}>
         <input type="text" placeholder="Username" name="username" />
         <button>Search</button>
       </form>
-      <div className="user">
-        <div className="detail">
-          <img src="./avatar.png" alt="" className="src" />
-          <span>Jane Doe</span>
+      {user && (
+        <div className="user">
+          <div className="detail">
+            <img src={user.avatar || "./avatar.png"} alt="" />
+            <span>{user.username}</span>
+          </div>
+          <button>Add User</button>
         </div>
-        <button>Add User</button>
-      </div>
+      )}
     </div>
   );
 };
